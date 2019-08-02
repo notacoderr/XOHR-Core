dd79f0186cb9606b374bdfa14176dbe9ad4d8a07
Clik <47439649+LegoFan48737@users.noreply.github.com>
2019-08-02 15:26:17 -0400

    All i can see is failure for Minigames now.......

--- src/Clik/MGCore/Main.php
+++ src/Clik/MGCore/Main.php
@@ -27,8 +27,7 @@ public function onEnable(){
     }
     public function onJoin(PlayerJoinEvent $event) {
         $player = $event->getPlayer();
-        $name = $player->getName();
-        $event->setJoinMessage("§0• §7[§b+§7]§f " . $name);
+        $event->setJoinMessage("");
         $level = $this->getServer()->getLevelByName("world");
         $x = -259;
         $y = 84;
@@ -37,12 +36,105 @@ public function onEnable(){
         $player->teleport($pos);
     }
     public function onQuit(PlayerQuitEvent $event) {
-        $player = $event->getPlayer();
-        $name = $player->getName();
-        $event->setQuitMessage("§0• §7[§b-§7]§f " . $name);
+        $event->setQuitMessage("");
     }
     public function onDeath(PlayerDeathEvent $event) {
-        $player = $event->getPlayer();
-        $name = $player->getName();
-        $event->setDeathMessage("§0• §7[§4X§7]§f " . $name);
+        $event->setDeathMessage("");
     }
+    public function onRespawn(PlayerRespawnEvent $event) {
+        $level = $this->getServer()->getLevelByName("world");
+        $x = -259;
+        $y = 82;
+        $z = -302;
+        $pos = new Position($x, $y, $z, $level);
+        $event->setRespawnPosition($pos);
+    }
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool
+    {
+        if($cmd->getName() == "gmc") {
+            if($sender instanceof Player) {
+                if($sender->hasPermission("orioncore.gmc.use")) {
+                    $sender->setGamemode(1);
+                    $sender->sendMessage($this->fts . TF::GREEN . "Your gamemode has been set to creative!");
+                } else {
+                    $sender->sendMessage($this->fts . TF::RED . "An error has occurred. Please contact Clik#3845 on Discord to report this");    
+                }
+            }
+        }
+        if($cmd->getName() == "gms") {
+            if($sender instanceof Player) {
+                if($sender->hasPermission("orioncore.gms.use")) {
+                    $sender->setGamemode(0);
+                    $sender->sendMessage($this->fts . TF::GREEN . "Your gamemode has been set to Survival!");
+                } else {
+                    $sender->sendMessage($this->fts . TF::RED . "An error has occurred. Please contact Clik#3845 on Discord to report this");
+                }
+            }
+        }
+        if($cmd->getName() == "gma") {
+            if($sender instanceof Player) {
+                if($sender->hasPermission("orioncore.gma.use")) {
+                    $sender->setGamemode(2);
+                    $sender->sendMessage($this->fts . TF::GREEN . "Your gamemode has been set to Adventure!");
+                } else {
+                    $sender->sendMessage($this->fts . TF::RED . "An error has occurred. Please contact Clik#3845 on Discord to report this");
+                }
+            }
+        }
+        if($cmd->getName() == "gmspc") {
+            if($sender instanceof Player) {
+                if($sender->hasPermission("orioncore.gmspc.use")) {
+                    $sender->setGamemode(3);
+                    $sender->sendMessage($this->fts . TF::GREEN . "Your gamemode has been set to Spectator!");
+                } else {
+                    $sender->sendMessage($this->fts . TF::RED . "An error has occurred. Please contact Clik#3845 on Discord to report this");
+                }
+            }
+        }
+        if($cmd->getName() == "day") {
+            if($sender instanceof Player) {
+                if($sender->hasPermission("orioncore.day.use")) {
+                    $sender->getLevel()->setTime(6000);
+                    $sender->sendMessage($this->fts . TF::GREEN . "Set the time to Day (6000) in your world!");
+                } else {
+                    $sender->sendMessage($this->fts . TF::RED . "An error has occurred. Please contact Clik#3845 on Discord to report this");
+                }
+            }
+        }
+        if($cmd->getName() == "night") {
+            if($sender instanceof Player) {
+                if($sender->hasPermission("orioncore.night.use")) {
+                    $sender->getLevel()->setTime(16000);
+                    $sender->sendMessage($this->fts . TF::GREEN . "Set the time to Night (16000) in your world!");
+                } else {
+                    $sender->sendMessage($this->fts . TF::RED . "An error has occurred. Please contact Clik#3845 on Discord to report this");
+                }
+            }
+        }
+        if($cmd->getName() == "hub") {
+            if($sender instanceof Player) {
+                $level = $this->getServer()->getLevelByName("world");
+                $x = -18;
+                $y = 66;
+                $z = 0;
+                $pos = new Position($x, $y, $z, $level);
+                $sender->teleport($pos);
+                $sender->sendMessage($this->fts . TF::GOLD . "Teleported to Hub");
+                $sender->setGamemode(0);
+            } else {
+                $sender->sendMessage($this->fts . TF::RED . "An error has occurred. Please contact Clik#3845 on Discord to report this");
+            }
+        }
+        if($cmd->getName() == "rules") {
+            if($sender instanceof Player) {
+                $sender->sendMessage("§6§o§lOrion Minigames Rules§r");
+                $sender->sendMessage("§f- §eNo Advertising");
+                $sender->sendMessage("§f- §eNo NSFW");
+                $sender->sendMessage("§f- §eNo cursing. (Censoring words is allowed.)");
+                $sender->sendMessage("§f- §eNo asking for OP/Ranks/Perms");
+                $sender->sendMessage("§f- §eUse Common Sense. Failure to do so will not exempt you from punishment.");
+            }
+        }
+    return true;
+    }
+}
